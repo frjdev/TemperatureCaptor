d110a1d16bbcc816cb336f54f07ba13a187473cf
Faraj <faraj.al.btadini@carbon-it.com>
2023-02-20 23:21:35 +0100

    feat: get the last 15 temperatures, (TDD red state)

--- test/Temperature.Domain.Tests/TemperatureServiceTest.cs
+++ test/Temperature.Domain.Tests/TemperatureServiceTest.cs
@@ -16,4 +16,18 @@ public class TemperatureServiceTest
 
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public async Task ShouldBeAbleToReturnTheHisotricsOfLast15Temperature()
+    {
+        var expected = new List<Temperature>();
+
+        var mockTemperatureRepo = new Mock<ITemperatureRepository>();
+        mockTemperatureRepo.Setup(x => x.GetHistoricTempAsync()).ReturnsAsync(expected);
+
+        var temp = new TemperatureService(mockTemperatureRepo.Object);
+        var actual = await temp.GetHistoricTempAsync().ConfigureAwait(false);
+
+        Assert.Equal(expected, actual);
+    }
 }
