64b8ed0214580600fbd91600b133b1c84333d58e
Faraj <faraj.al.btadini@carbon-it.com>
2023-02-21 13:27:24 +0100

    feat: update range for Hot state (integration test)

--- test/Temperature.Infrastructure.Tests/TemperatureRepositoryTests.cs
+++ test/Temperature.Infrastructure.Tests/TemperatureRepositoryTests.cs
@@ -33,4 +33,27 @@ public class TemperatureRepositoryTests
             Assert.Equal(15, actual.Count);
         }
     }
+    [Fact]
+    public async void ShouldBeAbleToUpdateHotRangeTemperature()
+    {
+        var options = BuildSqLiteDatabaseWithInitialData();
+        var expectedStart = 35;
+        var expectedEnd = 60;
+
+        await using var temperatureContext = new TemperatureContext(options);
+        {
+            var temperatureRepository = new TemperatureRepository(temperatureContext, new TemperatureCaptorGenerator());
+
+            var result = await temperatureRepository.UpdateRangeStateAsync("HOT", expectedStart, expectedEnd);
+
+            Assert.True(result);
+        }
+
+        await using var verifyTemperatureContext = new TemperatureContext(options);
+        {
+            var warmTemperaturesRange = await verifyTemperatureContext.TemperatureRangeSet.FirstOrDefaultAsync(x => x.State == "WARM");
+
+            Assert.Equal(expectedStart, warmTemperaturesRange!.End);
+        }
+    }
 }
