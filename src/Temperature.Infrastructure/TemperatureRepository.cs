cc2a8e0b74100d2dfc5242c00fb649cde03e1c0f
Faraj <faraj.al.btadini@carbon-it.com>
2023-02-21 10:46:18 +0100

    feat: update all range of all states into database

--- src/Temperature.Infrastructure/TemperatureRepository.cs
+++ src/Temperature.Infrastructure/TemperatureRepository.cs
@@ -48,12 +48,20 @@ public class TemperatureRepository : ITemperatureRepository
         return "HOT";
     }
 
-    public async Task<bool> UpdateRangeStateAsync(int idstate, double start, double end)
+    public async Task<bool> UpdateRangeStateAsync(string state, double start, double end)
     {
-        var range = _Context.TemperatureRangeSet!.FirstOrDefault(x => x.Id == idstate);
+        var result = await UpdateRangeState(state, start, end).ConfigureAwait(false);
+
+        return !result ? false : await UpdateOthersStates(state).ConfigureAwait(false);
+    }
+    private async Task<bool> UpdateRangeState(string state, double start, double end)
+    {
+        var range = _Context.TemperatureRangeSet!.FirstOrDefault(x => x.State == state);
 
         if (range == null)
+        {
             return false;
+        }
 
         range.Start = start;
         range.End = end;
@@ -63,6 +71,37 @@ public class TemperatureRepository : ITemperatureRepository
         var writtenStateEntries = await _Context.SaveChangesAsync().ConfigureAwait(false);
 
         return writtenStateEntries == 1;
+    }
+    private async Task<bool> UpdateOthersStates(string state)
+    {
+        var states = await GetStates().ConfigureAwait(false);
+
+        var warm = states.FirstOrDefault(x => x.State!.ToUpper() == "WARM");
+        var cold = states.FirstOrDefault(x => x.State!.ToUpper() == "COLD");
+        var hot = states.FirstOrDefault(x => x.State!.ToUpper() == "HOT");
+        var result = false;
+
+        switch (state.ToUpper())
+        {
+            case "HOT":
+                result = await UpdateRangeState("WARM", warm!.Start, hot!.Start);
+                break;
+            case "COLD":
+                result = await UpdateRangeState("WARM", cold!.Start, warm!.End);
+                break;
+            case "WARM":
+                result = await UpdateRangeState("HOT", warm!.End, hot!.End);
+                if (result == false)
+                    return false;
+
+                result = await UpdateRangeState("COLD", warm!.Start, cold!.End);
+                break;
+            default:
+                return false;
+        };
+
+        return result;
+
     }
     private async Task<IEnumerable<TemperatureRange>> GetStates()
     {
