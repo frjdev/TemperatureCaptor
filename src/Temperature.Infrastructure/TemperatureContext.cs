6a805e3f0447620ed9de1216e1598e8232ba8031
Faraj <faraj.al.btadini@carbon-it.com>
2023-02-21 01:15:49 +0100

    feat: add Model creating to seed database

--- src/Temperature.Infrastructure/TemperatureContext.cs
+++ src/Temperature.Infrastructure/TemperatureContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Temperature.Domain;
 
 namespace Temperature.Infrastructure;
 public sealed class TemperatureContext : DbContext
@@ -18,5 +19,14 @@ public sealed class TemperatureContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TemperatureData>().HasData(new TemperatureData { Id = 1, State = "COLD", Temp = 4, Date = DateTime.Now });
+        modelBuilder.Entity<TemperatureData>().HasData(new TemperatureData { Id = 2, State = "HOT", Temp = 41, Date = DateTime.Now });
+
+        modelBuilder.Entity<TemperatureRange>().HasData(new TemperatureDataRange { Id = 1, State = "HOT", Start = 40, End = 60 });
+        modelBuilder.Entity<TemperatureRange>().HasData(new TemperatureDataRange { Id = 2, State = "COLD", Start = 22, End = -50 });
+        modelBuilder.Entity<TemperatureRange>().HasData(new TemperatureDataRange { Id = 3, State = "WARM", Start = 22, End = 40 });
     }
 }
